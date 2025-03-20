2a2d92b4163c98227a338e2afa67560eac04d6bf
Alberto Camarena <albertocamarena@Albertos-iMac.local>
2025-03-20 10:57:12 -0600

    sync(cic): just sync

---
+++ tests/plate.py
@@ -0,0 +1,40 @@
+import genesis as gs
+import numpy as np
+
+def main():
+    gs.init(backend=gs.cpu)
+
+    scene = gs.Scene(
+        show_viewer = False,
+    )
+
+    floor = scene.add_entity(
+        gs.morphs.Plane(),
+    )
+
+    plate = scene.add_entity(
+        gs.morphs.Box(
+            size=(1.0, 1.0, 0.1),
+        ),
+    )
+
+    cam = scene.add_camera(
+        res    = (640, 480),
+        pos    = (3.5, 0.0, 2.5),
+        lookat = (0, 0, 0.5),
+        fov    = 30,
+        GUI    = False,
+    )
+
+    scene.build()
+    gs.tools.run_in_another_thread(fn=run_sim, args=(scene, cam))
+    cam.start_recording()
+
+def run_sim(scene, cam):
+    for i in range(120):
+        scene.step()
+        cam.render()
+    cam.stop_recording(save_to_filename='out/plate.mp4', fps=60)
+
+if __name__ == "__main__":
+    main()
